4d274e62c8b383a6d0311a66bc97d21348686a15
VincSP <vincent.serepeyrigain@viacesi.fr>
2017-06-23 13:34:11 +0200

    generate test of the view

---
+++ boulderdash/view/src/test/java/view/BoulderdashViewTest.java
@@ -0,0 +1,114 @@
+package view;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BoulderdashViewTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetGameMap() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetGameMap() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCloseView() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBoulderdashView() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDraw() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayMessage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBoulderdashViewIOrderPerformerIBoulderdashModelObservable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRun() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDiamondCount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTimeLeft() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoadMap() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCloseAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testKeyPressed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testKeyReleased() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testKeyTyped() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetOrderPerformer() {
+		fail("Not yet implemented");
+	}
+
+}
