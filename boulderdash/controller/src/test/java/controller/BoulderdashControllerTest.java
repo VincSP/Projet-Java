14e4edc12e4b424fcf8313a14e9d8b766bb62324
VincSP <vincent.serepeyrigain@viacesi.fr>
2017-06-23 11:30:16 +0200

    Test of the controller and model

---
+++ boulderdash/controller/src/test/java/controller/BoulderdashControllerTest.java
@@ -0,0 +1,64 @@
+package controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BoulderdashControllerTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testBoulderdashController() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPlay() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsAir() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSwapElements() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetView() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetModel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMoveMiner() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ boulderdash/model/src/test/java/model/BoulderdashModelTest.java
@@ -0,0 +1,84 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BoulderdashModelTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testBoulderdashModel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPlayerPosition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddMobile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveMobile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMobiles() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMobileByPlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMobilesHavesMoved() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOperation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddMobileIMobile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCurrentMap() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetElementByXY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetGameMapByLevel() {
+		fail("Not yet implemented");
+	}
+
+}
